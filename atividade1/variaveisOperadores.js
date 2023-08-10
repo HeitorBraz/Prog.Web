6db17c9f83a135479d850107874a82f9169a5923
HEITOR BRAZ REINO <heitor.reino@a.ucb.br>
2023-08-10 10:37:54 -0300

    Atividade 1 em sala

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+let num_1 = 3
+let num_2 = 6
+let num_3 = 9
+let num_4 = 12
+let num_5 = 15
+let num_6 = 18
+let num_7 = 21
+let num_8 = 24
+
+let res_Soma = num_1 + num_2
+let res_Sub = num_3 - num_4
+let res_Muilt = num_5 * num_6
+let res_Div = num_7 / num_8
+
+console.log("Resultado da Soma entre " + num_1 + " e " + num_2 + ": " + res_Soma)
+console.log("Resultado da Subtração entre " + num_3 + " e " + num_4 + ": " + res_Sub)
+console.log("Resultado da Muiltiplicação entre " + num_5 + " e " + num_6 + ": " + res_Muilt)
+console.log("Resultado da Divisão entre " + num_7 + " e " + num_8 + ": " + res_Div)
+
