01fddef7d37bd76c7132fc3bf376d238ad7232a6
Heitor <heitor.reino@a.ucb.br>
2023-10-05 10:57:31 -0300

    Atividade 6

--- atividade6/app.js
+++ atividade6/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -10,7 +15,30 @@ app.get('/', (req, res) => {
 });
 
 app.post('/dados', (req, res) => {
-    res.render('dados', {data: req.body});
+    let agendamento = {
+        nome: req.body.nome,
+        endereco: req.body.endereco,
+        telefone: req.body.telefone,
+        data_agendamento: req.body.data_agendamento
+    };
+
+    let erro_form = false;
+
+    if(agendamento.nome == ""){
+        erro_form = true;
+    }
+    if(agendamento.endereco == ""){
+        erro_form = true;
+    }
+    if(agendamento.telefone == ""){
+        erro_form = true;
+    }
+    if(agendamento.data_agendamento == ""){
+        erro_form = true;
+    }
+
+    res.render("dados.html", {agendamento, erro_form});
+
 });
 
 app.listen(3000, () => {
